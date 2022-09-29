0b5136090264a8c3b8751d315805af6dc571a40d
Lukasz Stysz <styszlukasz@gmail.com>
2022-09-29 20:08:26 +0200

    added classes in html, added rendering, added new tasks in js

--- script.js
+++ script.js
@@ -4,6 +4,10 @@
             content: "test",
             done: false
         },
+        {
+            content: "test-zrobione",
+            done: true
+        },
     ]
 
     const render = () => {
@@ -16,13 +20,32 @@
                 </li>
             `
         }
-        document.querySelector("js-taskList").innerHTML = htmlString;
+        document.querySelector(".js-taskList").innerHTML = htmlString;
     }
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        })
+    render()
+    }
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-addTask").value.trim()
+        if (newTaskContent === "") {
+            return
+        }
+        addNewTask(newTaskContent);
+    };
 
 
     const init = () => {
-
+        render();
+        const form = document.querySelector(".js-form");
+        form.addEventListener("submit", onFormSubmit)
     };
-    render()
+
     init()
 }
