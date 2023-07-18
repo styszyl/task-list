538b1b4461a6e3d867271c65171e9a3213640497
styszyl <97724595+styszyl@users.noreply.github.com>
2023-07-18 14:25:43 +0200

    added changing status on "ukryj konczone" button

--- script.js
+++ script.js
@@ -8,13 +8,13 @@
   };
 
   const removeTask = (index) => {
-    tasks = [...tasks.filter((task, i) => i != index)]
+    tasks = [...tasks.filter((task, i) => i != index)];
     render();
   };
 
   const toggleDoneTasks = (index) => {
     tasks = tasks.map((task, i) => {
-      return (i == index) ? { ...task, done: !task.done } : task
+      return i == index ? { ...task, done: !task.done } : task;
     });
     render();
   };
@@ -37,32 +37,44 @@
 
   const markDoneAllTasks = () => {
     tasks = tasks.map((task) => {
-      return { ...task, done: true }
-    })
+      return { ...task, done: true };
+    });
     render();
   };
 
   const bindButtonsEvents = () => {
-    const markDoneAllTasksButton = document.querySelector(".markAllDoneTasksButton");
+    const markDoneAllTasksButton = document.querySelector(
+      ".markAllDoneTasksButton"
+    );
     if (markDoneAllTasksButton != null) {
       markDoneAllTasksButton.addEventListener("click", () => {
         markDoneAllTasks();
-      })
-    };
-  }
-
-  const isTaskDone = (task) => task.done ? true : false
-
-  const isEveryTaskDone = () => {
-    const markDoneAllTasksButton = document.querySelector(".markAllDoneTasksButton");
-    if (markDoneAllTasksButton != null) {
-      if (tasks.every((task) => isTaskDone(task))) {
-        markDoneAllTasksButton.toggleAttribute("disabled")
-      };
-    };
+      });
+    }
   };
 
+  const isTaskDone = (task) => (task.done ? true : false);
 
+  const isEveryTaskDone = () => {
+    const markDoneAllTasksButton = document.querySelector(
+      ".markAllDoneTasksButton"
+    );
+    if (markDoneAllTasksButton != null) {
+      if (tasks.every((task) => isTaskDone(task))) {
+        markDoneAllTasksButton.toggleAttribute("disabled");
+      }
+    }
+  };
+
+  const togglehideDoneTasksStatus = () => {
+    hideDoneTasks = !hideDoneTasks;
+    render();
+  };
+
+  const hideAllDoneTasks = () => {
+    const hideDoneTasksButton = document.querySelector(".hideDoneTasksButton");
+    hideDoneTasksButton != null ? hideDoneTasksButton.addEventListener("click", () => {togglehideDoneTasksStatus()}) : renderButtons();
+  };
 
   const renderTasks = () => {
     let htmlString = "";
@@ -70,29 +82,32 @@
     for (const task of tasks) {
       htmlString += `
                 <li class="taskList__item">
-                    <button class="js-done taskList__checkbox taskList__checkbox--green ${task.done ? " taskList__checkbox--greenDone" : " "
-        }">✔</button>
-                    <div class="taskList__content ${task.done ? " taskList__content--done" : " "
-        }">${task.content} \n </div>
+                    <button class="js-done taskList__checkbox taskList__checkbox--green ${
+                      task.done ? " taskList__checkbox--greenDone" : " "
+                    }">✔</button>
+                    <div class="taskList__content ${
+                      task.done ? " taskList__content--done" : " "
+                    }">${task.content} \n </div>
                     <button class="js-deleteButton taskList__checkbox taskList__checkbox--deleteButton">🗑</button>
                 </li>
             `;
     }
     document.querySelector(".js-taskList").innerHTML = htmlString;
-
-  }
+  };
 
   const renderButtons = () => {
     let buttonsHTML = " ";
 
     if (tasks.length >= 1) {
       buttonsHTML += ` 
-      <button class="tasklistButton hideDoneTasksButton">Ukryj ukończone</button>
+      <button class="tasklistButton hideDoneTasksButton">${
+        hideDoneTasks ? "Odkryj ukończone" : "Ukryj ukończone"
+      }</button>
       <button class="tasklistButton markAllDoneTasksButton">Ukończ wszystkie</button>
     `;
     }
     document.querySelector(".js-tasklistButtons").innerHTML = buttonsHTML;
-  }
+  };
 
   const render = () => {
     renderTasks();
@@ -100,6 +115,7 @@
     bindEvents();
     bindButtonsEvents();
     isEveryTaskDone();
+    hideAllDoneTasks();
   };
 
   const onFormSubmit = (event) => {
